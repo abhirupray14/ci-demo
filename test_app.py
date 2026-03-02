3b990c1b6351ae586f06b2d306e154c5189ae47d
Abhirup Ray <49801323+abhirupray14@users.noreply.github.com>
2026-03-02 12:39:02 +0530

    Test job added3

--- test_app.py
+++ test_app.py
@@ -3,6 +3,7 @@ from app import add, sub
 
 
 class TestMathFunctions(unittest.TestCase):
+    
 
     def test_add(self):
         self.assertEqual(add(5, 3), 8)
@@ -18,4 +19,3 @@ class TestMathFunctions(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main() 
-      
