142702d19aeaeedc43a039a0978fae5d33c4860c
Abhirup Ray <49801323+abhirupray14@users.noreply.github.com>
2026-03-02 12:33:37 +0530

    Testing job added

--- test_app.py
+++ test_app.py
@@ -8,10 +8,12 @@ class TestMathFunctions(unittest.TestCase):
         self.assertEqual(add(-1, 1), 0)
         self.assertEqual(add(0, 0), 0)
 
+
     def test_sub(self):
         self.assertEqual(sub(5, 3), 2)
         self.assertEqual(sub(-1, 1), -2)
         self.assertEqual(sub(0, 0), 0)
+
 
 if __name__ == '__main__':
     unittest.main() 
